8df70ff8e665a25b1ad9794ff94577181f51cff5
kamruzzaman-leeon <leeon.ewubd@gmail.com>
2023-10-14 13:39:26 +0600

    login & registerpage added

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root from "./components/Root";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
-import './index.css'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Root></Root>,
+    children:[
+      {
+        path:"/",
+        element:<Home></Home>
+      },
+      {
+        path:"/login",
+        element: <Login></Login>
+      },
+      {
+        path:"/register",
+        element:<Register></Register>
+      }
+    ]
+  },
+]);
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
